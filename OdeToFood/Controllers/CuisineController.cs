a644248ced87c5d4ab8f2d5f254925492fbd0851
VMir_MK <VMir_MK@Europe.com>
2012-12-24 16:40:12 +0100

    Action Results
    Different types of ActionResult.

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -15,10 +15,21 @@ namespace OdeToFood.Controllers
         /// </summary>
         /// <param name="name">Parameter with default value</param>
         /// <returns></returns>
-        public ActionResult Search(string name="Macedonian")
+        public ActionResult Search(string name="*")
         {
+            if (name == "*")
+            {
+                //return Json(new {Cuisinename = name}, JsonRequestBehavior.AllowGet);
+                //return File(Server.MapPath("~/Content/Site.css"), "Site.css");
+                return RedirectToRoute("Cuisine", new { name = "Macedonian" });
+                //return RedirectToActionPermanent("Search", "Cuisine", new { name = "french" });
+                //return RedirectToAction("Search", "Cuisine", new {name = "french"});
+            }
+
             name = Server.HtmlEncode(name);
             return Content(name);
+            //return RedirectPermanent("http://uk.eurosport.yahoo.com");
+            //return RedirectToAction("Index", "Home");
         }
 
     }
