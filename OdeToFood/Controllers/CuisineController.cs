fe00e8df701407193d5a6c1549d321106b89ee32
VMir_MK <VMir_MK@Europe.com>
2012-12-25 10:14:34 +0100

    Action filters
    Applying Action filters to controllers, action and globaly
    Handling errors on client machine (web.config)
    Adding custom action filter

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OdeToFood.Infrastructure;
 
 namespace OdeToFood.Controllers
 {
@@ -15,20 +16,23 @@ namespace OdeToFood.Controllers
         /// </summary>
         /// <param name="name">Parameter with default value</param>
         /// <returns></returns>
+        [HttpGet]
+        //[Authorize]
+            [Log]
         public ActionResult Search(string name="*")
         {
-            if (name == "*")
-            {
-                //return Json(new {Cuisinename = name}, JsonRequestBehavior.AllowGet);
-                //return File(Server.MapPath("~/Content/Site.css"), "Site.css");
-                return RedirectToRoute("Cuisine", new { name = "Macedonian" });
-                //return RedirectToActionPermanent("Search", "Cuisine", new { name = "french" });
-                //return RedirectToAction("Search", "Cuisine", new {name = "french"});
-            }
+            throw new Exception("kablaaaam!");
+            //if (name == "*")
+            //{
+            //    //return Json(new {Cuisinename = name}, JsonRequestBehavior.AllowGet);
+            //    //return File(Server.MapPath("~/Content/Site.css"), "Site.css");
+            //    return RedirectToRoute("Cuisine", new { name = "Macedonian" });
+            //    //return RedirectToActionPermanent("Search", "Cuisine", new { name = "french" });
+            //    //return RedirectToAction("Search", "Cuisine", new {name = "french"});
+            //}
 
             name = Server.HtmlEncode(name);
             return Content(name);
-            //return RedirectPermanent("http://uk.eurosport.yahoo.com");
             //return RedirectToAction("Index", "Home");
         }
 
