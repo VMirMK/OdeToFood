bac804b9a907ef2193642d52e11a1ae02f52feaa
VMirMK <VMir_MK@europe.com>
2013-01-11 16:44:45 +0100

    Setting up ASP.NET MVC 3 to work with jQuery UI
    Update of jQuery with NuGet (Update-Package jQuery)

--- OdeToFood/Scripts/OdeToFood.js
+++ OdeToFood/Scripts/OdeToFood.js
@@ -1,10 +1,10 @@
-﻿/// <reference path="jquery-1.5.1-vsdoc.js" />
+﻿/// <reference path="jquery-1.8.3-vsdoc.js" />
 /// <reference path="jquery-ui-1.8.11.js" />
 
 $(document).ready(function () {
 
     //    $(".autocomplete").autocomplete(); class="autocomplete"
-    $(":inut[data-autocomplete]").each(function () {
+    $(":input[data-autocomplete]").each(function () {
         $(this).autocomplete({ source: $(this).attr("data-autocomplete") });
     });
 })
